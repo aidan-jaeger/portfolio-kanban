e5340bcab63d0d502415473afc1f8cb830ded4dd
Aidan Jaeger <ejaeger0807@gmail.com>
2026-09-16 12:22:18 -0400

    entry point boilerplate

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import http from 'node:http'
+import router from './router.js'
+import JsonView from './views/jsonView.js'
+
+const PORT = process.env.port || 3500
+
+const middlewares = [
+  //logger
+  (req, res, next) => {
+    console.log(`${new Date().toISOString()}\t${req.method}\t${req.url}`)
+    rext()
+  },
+]
+
+const server = http.createServer((req, res) => {
+  let index = 0
+
+  function next() {
+    if (index >= middlewares.length)
+      return JsonView(res, 404, 'Resource Not Found')
+
+    const currentMiddleware = middlewares[index++]
+    currentMiddleware(req, res, next)
+  }
+  
+  next()
+})
+
+server.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
---
+++ server/views/jsonView.js
@@ -0,0 +1,13 @@
+const JsonView = {
+  // Format successful data responses
+  render: (res, statusCode, data) => {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(data))
+  },
+
+  // Format error payloads uniformly
+  renderError: (res, statusCode, message) => {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ error: message }))
+  }
+}

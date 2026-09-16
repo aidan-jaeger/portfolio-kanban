1a6aa6e1028af67c7dcc23844a1fd240282ab939
Aidan Jaeger <ejaeger0807@gmail.com>
2026-09-16 16:42:46 -0400

    fixed writeTempFile

--- server/utils/writeTempFile.js
+++ server/utils/writeTempFile.js
@@ -3,20 +3,22 @@ import os from 'node:os'
 import path from 'node:path'
 import crypto from 'node:crypto'
 
-export async function writeTempFile(data) {
+export async function writeTempFile(data, attempt = 1) {
+  if (attempt > 3) throw new Error('Max retries exceeded @ writeTempFile')
+
   const uniqueName = crypto.randomBytes(16).toString('hex')
-  const tempPath = await fs.mkdtemp(path.join(os.tmpdir(), `kanban-${uniqueName}.tmp`))
+  const tempFilePath = path.join(os.tmpdir(), `kanban-${uniqueName}.json`)
 
   try {
-    await fs.writeFile(tempPath, data, { flag: 'wx', mode: 0o600 })
-    console.log(`Wrote to temp file: ${tempPath}`)
+    await fs.writeFile(tempFilePath, data, { flag: 'wx', mode: 0o600 })
+    console.log(`Wrote to temp file: ${tempFilePath}`)
     
-    return tempPath //make sure to clean up tmp file after use w/ fs.rm(isolatedDir, { recursive: true, force: true })
+    return tempFilePath //make sure to clean up tmp file after use w/ fs.rm(isolatedDir, { recursive: true, force: true })
   } 
-  catch {
+  catch(err) {
     if (err.code === 'EEXIST') {
       console.warn('File collision detected, retrying with new name')
-      return await writeTempFile(data)
+      return await writeTempFile(data, attempt++)
     }
     throw err
   } 

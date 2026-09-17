84ac893a27329c7521e059402d31d6a8ef6ffe63
Aidan Jaeger <ejaeger0807@gmail.com>
2026-09-17 18:51:17 -0400

    added directory specification to writeTempFile, no longer uses OS temp dir

--- server/utils/writeTempFile.js
+++ server/utils/writeTempFile.js
@@ -1,24 +1,23 @@
 import fs from 'node:fs/promises'
-import os from 'node:os'
 import path from 'node:path'
 import crypto from 'node:crypto'
 
-export async function writeTempFile(data, attempt = 1) {
+export default async function writeTempFile(dir, data, attempt = 1) {
   if (attempt > 3) throw new Error('Max retries exceeded @ writeTempFile')
 
   const uniqueName = crypto.randomBytes(16).toString('hex')
-  const tempFilePath = path.join(os.tmpdir(), `kanban-${uniqueName}.json`)
+  const tempFilePath = path.join(dir, `kanban-${uniqueName}.tmp`)
 
   try {
     await fs.writeFile(tempFilePath, data, { flag: 'wx', mode: 0o600 })
     console.log(`Wrote to temp file: ${tempFilePath}`)
     
-    return tempFilePath //make sure to clean up tmp file after use w/ fs.rm(isolatedDir, { recursive: true, force: true })
+    return tempFilePath //make sure to clean up tmp file after use w/ fs.rm(tempFilePath, { force: true })
   } 
   catch(err) {
     if (err.code === 'EEXIST') {
       console.warn('File collision detected, retrying with new name')
-      return await writeTempFile(data, attempt + 1)
+      return await writeTempFile(dir, data, attempt + 1)
     }
     throw err
   } 

64e446962f36e5218bcc7313bd728e489d2a4d6b
Aidan Jaeger <ejaeger0807@gmail.com>
2026-09-20 12:56:56 -0400

    fixed typo "rext()" in console log middleware, made middleware handler "next()" async to handle async errors

--- server/server.js
+++ server/server.js
@@ -2,28 +2,33 @@ import http from 'node:http'
 import router from './router.js'
 import JsonView from './views/jsonView.js'
 
-const PORT = process.env.port || 3500
+const PORT = process.env.PORT || 3500
 
 const middlewares = [
   //logger
   (req, res, next) => {
     console.log(`${new Date().toISOString()}\t${req.method}\t${req.url}`)
-    rext()
+    next()
   },
   router,
 ]
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   let index = 0
 
-  function next() {
+  async function next() {
     if (index >= middlewares.length)
       return JsonView(res, 404, 'Resource Not Found')
 
     const currentMiddleware = middlewares[index++]
-    currentMiddleware(req, res, next)
+    try {
+      await currentMiddleware(req, res, next)
+    }
+    catch(err) {
+      console.error(`Server error: ${err}`)
+      //error view here
+    }
   }
-  
   next()
 })
 

27d12919c9b0587da0519471a222243e2a35121c
Aidan Jaeger <ejaeger0807@gmail.com>
2026-09-24 13:35:00 -0400

    removed config comments for hashPassword

--- server/utils/hashVerify.js
+++ server/utils/hashVerify.js
@@ -3,9 +3,9 @@ import argon2 from 'argon2'
 export async function hashPassword(password) {
   return await argon2.hash(password, {
     type: argon2.argon2id,
-    memoryCost: 65536, // 64 MiB
-    timeCost: 3,       // Iterations
-    parallelism: 4     // Threads
+    memoryCost: 65536,
+    timeCost: 3,
+    parallelism: 4
   });
 }
 
